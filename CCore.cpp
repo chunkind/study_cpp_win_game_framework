31c72cbb0d8a0365e5ca6b811432afdcfeeac075
chunkind <chunkind@naver.com>
2025-09-16 18:08:03 +0900

    8/16: - 윈도우 함수 표시

--- CCore.cpp
+++ CCore.cpp
@@ -25,7 +25,7 @@ int CCore::init(HWND _hWnd, POINT _ptResultution)
 	::AdjustWindowRect(&rt, WS_OVERLAPPEDWINDOW, true);
 	::SetWindowPos(m_hWnd, nullptr, 100, 100, rt.right - rt.left, rt.bottom - rt.top, 0);
 
-	m_hDC = GetDC(m_hWnd);
+	m_hDC = ::GetDC(m_hWnd);
 
 	g_obj.SetPos(Vec2((float)(m_ptResolution.x / 2), (float)(m_ptResolution.y / 2)));
 	g_obj.SetScale(Vec2(100, 100));
@@ -43,12 +43,12 @@ void CCore::update()
 {
 	Vec2 vPos = g_obj.GetPos();
 
-	if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+	if (::GetAsyncKeyState(VK_LEFT) & 0x8000)
 	{
 		vPos.x -= 0.01f;
 	}
 
-	if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
+	if (::GetAsyncKeyState(VK_RIGHT) & 0x8000)
 	{
 		vPos.x += 0.01f;
 	}
@@ -61,7 +61,7 @@ void CCore::render()
 	Vec2 vPos = g_obj.GetPos();
 	Vec2 vScale = g_obj.GetScale();
 
-	Rectangle(m_hDC
+	::Rectangle(m_hDC
 		, int(vPos.x - vScale.x / 2.f)
 		, int(vPos.y - vScale.y / 2.f)
 		, int(vPos.x + vScale.x / 2.f)
