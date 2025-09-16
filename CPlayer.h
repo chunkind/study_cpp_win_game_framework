95022850c612619174eb2f08bbdf5a79ed68d019
chunkind <chunkind@naver.com>
2025-09-16 21:38:50 +0900

    9/16: - Player 분리 - 방향키이동은 Player 만

---
+++ CPlayer.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "CObject.h"
+
+class CPlayer : public CObject
+{
+public:
+    virtual void update();
+};
--- CScene_Start.cpp
+++ CScene_Start.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CScene_Start.h"
 #include "CObject.h"
+#include "CPlayer.h"
 
 CScene_Start::CScene_Start()
 {
@@ -12,14 +13,18 @@ CScene_Start::~CScene_Start()
 
 void CScene_Start::Enter()
 {
-	CObject* pObj = new CObject;
-
+	CObject* pObj = new CPlayer;
 	pObj->SetPos(Vec2(640.f, 384.f));
 	pObj->SetScale(Vec2(100.f, 100.f));
+	AddObject(pObj, GROUP_TYPE::DEFAULT);
 
+	pObj = new CObject;
+	pObj->SetPos(Vec2(640.f, 384.f));
+	pObj->SetScale(Vec2(100.f, 100.f));
 	AddObject(pObj, GROUP_TYPE::DEFAULT);
 }
 
 void CScene_Start::Exit()
 {
+
 }
