b4238e37a2ada174c0b5c1fefcdbca9845dd0462
chunkind <chunkind@naver.com>
2025-09-16 22:00:51 +0900

    9/16: - 미사일 이동(백터)

--- CMissile.cpp
+++ CMissile.cpp
@@ -5,8 +5,9 @@
 
 CMissile::CMissile()
 	: m_fTheta(PI / 2.f)
+	, m_vDir(Vec2(1.f, -1.f))
 {
-
+	m_vDir.Normalize();
 }
 
 CMissile::~CMissile()
@@ -17,8 +18,8 @@ void CMissile::update()
 {
 	Vec2 vPos = GetPos();
 
-	vPos.x += 600.f * cosf(m_fTheta) * fDT;
-	vPos.y -= 600.f * sinf(m_fTheta) * fDT;
+	vPos.x += 600.f * m_vDir.x * fDT;
+	vPos.y += 600.f * m_vDir.y * fDT;
 
 	SetPos(vPos);
 }
