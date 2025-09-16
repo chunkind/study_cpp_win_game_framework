bd668ed7ec986702dbcf64237269d331a5e1c7dc
chunkind <chunkind@naver.com>
2025-09-16 22:53:04 +0900

    9/16: - 마젠타 색 이그노어

--- CPlayer.cpp
+++ CPlayer.cpp
@@ -59,12 +59,15 @@ void CPlayer::render(HDC _dc)
 
 	Vec2 vPos = GetPos();
 
-	BitBlt(_dc
+	TransparentBlt(_dc
 		, int(vPos.x - (float)(iWidth / 2))
 		, int(vPos.y - (float)(iHeight / 2))
 		, iWidth, iHeight
 		, m_pTex->GetDC()
-		, 0, 0, SRCCOPY);
+		, 0, 0 
+		, iWidth, iHeight
+		, RGB(255, 0, 255)
+	);
 }
 
 void CPlayer::CreateMissile()
@@ -75,7 +78,7 @@ void CPlayer::CreateMissile()
 	CMissile* pMissile = new CMissile;
 	pMissile->SetPos(vMissilePos);
 	pMissile->SetScale(Vec2(25.f, 25.f));
-	pMissile->SetDir(Vec2(-1.f, -7.f));
+	pMissile->SetDir(Vec2(0.f, -1.f));
 
 	CScene* pCurScene = CSceneMgr::GetInst()->GetCurScene();
 	pCurScene->AddObject(pMissile, GROUP_TYPE::DEFAULT);
