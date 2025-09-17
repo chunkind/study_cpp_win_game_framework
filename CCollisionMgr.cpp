54509feeb9a7a3eb4f3c45d4e5a563be663b84c3
chunkind <chunkind@naver.com>
2025-09-17 18:05:53 +0900

    9/17: - 충돌로직 구현

--- CCollisionMgr.cpp
+++ CCollisionMgr.cpp
@@ -1,5 +1,9 @@
 #include "pch.h"
 #include "CCollisionMgr.h"
+#include "CSceneMgr.h"
+#include "CScene.h"
+#include "CObject.h"
+#include "CCollider.h"
 
 CCollisionMgr::CCollisionMgr()
 	: m_arrCheck{}
@@ -14,10 +18,74 @@ CCollisionMgr::~CCollisionMgr()
 
 void CCollisionMgr::update()
 {
+	for (UINT iRow = 0; iRow < (UINT)GROUP_TYPE::END; ++iRow)
+	{
+		for (UINT iCol = 0; iCol < (UINT)GROUP_TYPE::END; ++iCol)
+		{
+			if (m_arrCheck[iRow] & (1 << iCol))
+			{
+				CollisionGroupUpdate((GROUP_TYPE)iRow, (GROUP_TYPE)iCol);
+			}
+		}
+	}
+}
+
+void CCollisionMgr::CollisionGroupUpdate(GROUP_TYPE _eLeft, GROUP_TYPE _eRight)
+{
+	CScene* pCurScene = CSceneMgr::GetInst()->GetCurScene();
+
+	const vector<CObject*>& vecLeft = pCurScene->GetGroupObject(_eLeft);
+	const vector<CObject*>& vecRight = pCurScene->GetGroupObject(_eRight);
+
+	for (size_t i = 0; i < vecLeft.size(); ++i)
+	{
+		if (nullptr == vecLeft[i]->GetCollider())
+		{
+			continue;
+		}
+
+		for (size_t j = 0; j < vecRight.size(); ++j)
+		{
+			if (nullptr == vecRight[j]->GetCollider()
+				|| vecLeft[i] == vecRight[j])
+			{
+				continue;
+			}
+
+			if (IsCollision(vecLeft[i]->GetCollider(), vecRight[j]->GetCollider()))
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
+
+bool CCollisionMgr::IsCollision(CCollider* _pLeftCol, CCollider* _pRightCol)
+{
+	return false;
 }
 
 void CCollisionMgr::CheckGroup(GROUP_TYPE _eLeft, GROUP_TYPE _eRight)
 {
-	// 더 작은 값의 그룹 타입을 행으로,
-	// 큰 값을 열(비트) 로 사용
+	UINT iRow = (UINT)_eLeft;
+	UINT iCol = (UINT)_eRight;
+
+	if (iCol < iRow)
+	{
+		iRow = (UINT)_eRight;
+		iCol = (UINT)_eLeft;
+	}
+
+	if (m_arrCheck[iRow] & (1 << iCol))
+	{
+		m_arrCheck[iRow] &= ~(1 << iCol);
+	}
+	else
+	{
+		m_arrCheck[iRow] |= (1 << iCol);
+	}
 }
--- CCollisionMgr.h
+++ CCollisionMgr.h
@@ -1,14 +1,20 @@
 #pragma once
 
+class CCollider;
+
 class CCollisionMgr
 {
 	SINGLE(CCollisionMgr)
 
 private:
-	UINT	m_arrCheck[(UINT)GROUP_TYPE::END];
+	UINT m_arrCheck[(UINT)GROUP_TYPE::END];
 
 public:
 	void update();
 	void CheckGroup(GROUP_TYPE _eLeft, GROUP_TYPE _eRight);
 	void Reset() { memset(m_arrCheck, 0, sizeof(UINT) * (UINT)GROUP_TYPE::END); }
+
+private:
+	void CollisionGroupUpdate(GROUP_TYPE _eLeft, GROUP_TYPE _eRight);
+	bool IsCollision(CCollider* _pLeftCol, CCollider* _pRightCol);
 };
