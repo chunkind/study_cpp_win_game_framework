7d5dab1ba60735e0b4a8c969051705b1e2da34ef
chunkind <chunkind@naver.com>
2025-09-17 18:58:38 +0900

    9/17: - 이벤트 매니저로 오브잭트 삭제 처리 준비

--- CEventMgr.cpp
+++ CEventMgr.cpp
@@ -16,11 +16,22 @@ CEventMgr::~CEventMgr()
 
 void CEventMgr::update()
 {
+	// ===================================================
+	// 이전 프레임에서 등록해둔 Dead Object 들을 삭제한다.
+	// ===================================================
+	for (size_t i = 0; i < m_vecDead.size(); ++i)
+	{
+		delete m_vecDead[i];
+	}
+	m_vecDead.clear();
+
+	// ==========
+	// Event 처리
+	// ==========
 	for (size_t i = 0; i < m_vecEvent.size(); ++i)
 	{
 		Excute(m_vecEvent[i]);
 	}
-
 	m_vecEvent.clear();
 }
 
@@ -37,7 +48,11 @@ void CEventMgr::Excute(const tEvent& _eve)
 	}
 	break;
 	case EVENT_TYPE::DELETE_OBJECT:
-
+	{
+		CObject* pDeadObj = (CObject*)_eve.lParam;
+		pDeadObj->SetDead();
+		m_vecDead.push_back(pDeadObj);
+	}
 		break;
 	case EVENT_TYPE::SCENE_CHANGE:
 
