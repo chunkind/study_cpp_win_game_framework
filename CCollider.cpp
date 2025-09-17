25ce60838a58629e59d587f5d36b2fbdf2685adf
chunkind <chunkind@naver.com>
2025-09-17 13:07:50 +0900

    9/17: - 자주사용하는 펜 모듈화

--- CCollider.cpp
+++ CCollider.cpp
@@ -12,7 +12,6 @@ CCollider::~CCollider()
 {
 }
 
-
 void CCollider::finalupdate()
 {
 	Vec2 vObjectPos = m_pOwner->GetPos();
@@ -21,10 +20,19 @@ void CCollider::finalupdate()
 
 void CCollider::render(HDC _dc)
 {
-	Rectangle(_dc
+	HPEN hGreenPen = CCore::GetInst()->GetPen(PEN_TYPE::GREEN);
+	HPEN hDefaultPen = (HPEN)::SelectObject(_dc, hGreenPen);
+
+	HBRUSH hHolowBrush = CCore::GetInst()->GetBrush(BRUSH_TYPE::HOLLOW);
+	HBRUSH hDefaultBrush = (HBRUSH)::SelectObject(_dc, hHolowBrush);
+
+	::Rectangle(_dc
 		, (int)(m_vFinalPos.x - m_vScale.x / 2.f)
 		, (int)(m_vFinalPos.y - m_vScale.y / 2.f)
 		, (int)(m_vFinalPos.x + m_vScale.x / 2.f)
 		, (int)(m_vFinalPos.y + m_vScale.y / 2.f)
 	);
+
+	::SelectObject(_dc, hDefaultPen);
+	::SelectObject(_dc, hDefaultBrush);
 }
--- CCore.h
+++ CCore.h
@@ -3,6 +3,7 @@
 class CCore
 {
 	SINGLE(CCore)
+
 private:
 	HWND	m_hWnd;
 	POINT	m_ptResolution;
@@ -11,14 +12,21 @@ private:
 	HBITMAP	m_hBit;
 	HDC		m_memDC;
 
+	HBRUSH m_arrBrush[(UINT)BRUSH_TYPE::END];
+	HPEN m_arrPen[(UINT)PEN_TYPE::END];
+
 public:
 	int init(HWND _hWnd, POINT _ptResolution);
 	void progress();
 
 private:
+	void CreateBrushPen();
 
 public:
 	HWND GetMainHwnd() { return m_hWnd; }
 	HDC GetMainDC() { return m_hDC; }
 	POINT GetResolution() { return m_ptResolution; }
+
+	HBRUSH GetBrush(BRUSH_TYPE _eType) { return m_arrBrush[(UINT)_eType]; }
+	HPEN GetPen(PEN_TYPE _eType) { return m_arrPen[(UINT)_eType]; }
 };
