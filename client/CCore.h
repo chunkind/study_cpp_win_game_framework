c3dcafafa7fbdc8d7cb96490610d2a228e95674e
chunkind <chunkind@naver.com>
2025-09-28 17:40:52 +0900

    clear

--- client/CCore.h
+++ client/CCore.h
@@ -3,13 +3,10 @@
 class CCore
 {
 public:
-	// 정적변수로 싱글톤을 만들면 내가 해제를 신경쓰지 않아도 된다.
-	// 프로그램 꺼질때 자동 해제된다.
-	// 반대로 해제 되지 않아서 메모리에서 지울수가 없다.
 	static CCore* GetInst()
 	{
-		static CCore core; // 데이터 영역에 core를 만든거다.
-		return &core; // core라는 정적 변수는 해당 메서드에서만 접근 가능해도 데이터 영역에주소값은 접근 가능하다.
+		static CCore core;
+		return &core;
 	}
 
 private:
