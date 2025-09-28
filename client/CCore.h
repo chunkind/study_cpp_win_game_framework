63ce78dc1840330d0e7cc44b654c2ed7a74b52f4
chunkind <chunkind@naver.com>
2025-09-28 17:45:37 +0900

    9/28: - Define 생성

--- client/CCore.h
+++ client/CCore.h
@@ -1,13 +1,24 @@
 #pragma once
+//new
+#include "Define.h"
+#include <Windows.h>
 
 class CCore
 {
 public:
-	static CCore* GetInst()
+	//old
+	/*static CCore* GetInst()
 	{
 		static CCore core;
 		return &core;
-	}
+	}*/
+	//new
+	SINGLE(CCore)
+
+//new
+private:
+	HWND		m_hWnd;				// 메인 윈도우 핸들
+	POINT		m_ptResolution;		// 메인 윈도우 해상도
 
 private:
 	CCore();
